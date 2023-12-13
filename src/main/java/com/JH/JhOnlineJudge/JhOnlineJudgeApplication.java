07dec925675b46a4aa5288bbc093916de0479f8d
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2023-12-13 15:08:34 +0900

    SecurityAutoConfiguration.class 자동보안 종료 ( 인증때 고쳐야함)

--- src/main/java/com/JH/JhOnlineJudge/JhOnlineJudgeApplication.java
+++ src/main/java/com/JH/JhOnlineJudge/JhOnlineJudgeApplication.java
@@ -2,8 +2,9 @@ package com.JH.JhOnlineJudge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
 public class JhOnlineJudgeApplication {
 
 	public static void main(String[] args) {
